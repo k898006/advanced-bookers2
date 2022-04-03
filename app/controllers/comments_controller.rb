b400d6ac675aed08ff83a5199505a9b33d4ac125
k898006 <kazutoshi.saito225@gmail.com>
2022-04-03 07:20:17 +0000

    [add] favorite and comment function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,17 @@
 class CommentsController < ApplicationController
+
+
   def create
     book = Book.find(params[:book_id])
     comment = current_user.comments.new(comment_params)
     comment.book_id = book.id
     comment.save
     redirect_to book_path(book)
+  end
+
+  def destroy
+  Comment.find(params[:id]).destroy
+  redirect_to request.referer
   end
 
 
