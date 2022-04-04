b34fc4250fe578ac0b70d5424a6412d9ea02bfea
k898006 <kazutoshi.saito225@gmail.com>
2022-04-04 09:40:44 +0000

    [finish] advanced-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'favorites/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to:"homes#top"
@@ -11,7 +10,11 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update]
 
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
